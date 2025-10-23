df1eae1624c2d1a38c07b3c69fb16dc3bac43f50
khazink <47601685+khazink@users.noreply.github.com>
2025-10-23 18:32:54 +0800

    with makefile until itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -1,36 +1,54 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kkaman <kkaman@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/17 10:38:46 by kkaman            #+#    #+#             */
-/*   Updated: 2025/10/23 14:46:43 by kkaman           ###   ########.fr       */
+/*   Created: 2025/10/23 17:47:01 by kkaman            #+#    #+#             */
+/*   Updated: 2025/10/23 18:25:47 by kkaman           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*strdup(const char *s)
+static int	num_len(long n)
 {
-	char	*ptr;
-	int		len;
+	int	len;
 
-	if (s == NULL)
-		return (NULL);
-	len = 0;
-	while (s[len] != '\0')
-		len++;
-	ptr = malloc((len + 1) * sizeof(char));
-	if (!ptr)
-		return (NULL);
-	len = 0;
-	while (s[len] != '\0')
+	len = 1;
+	while (n / 10)
 	{
-		ptr[len] = s[len];
+		n = n / 10;
 		len++;
 	}
-	ptr[len] = '\0';
-	return (ptr);
+	return (len);
+}
+
+char	*ft_itoa(int n)
+{
+	int		sign;
+	char	*str;
+	int		len;
+	long	nb;
+
+	nb = n;
+	sign = (nb < 0);
+	if (nb < 0)
+		nb = -nb;
+	len = num_len(nb) + sign;
+	str = malloc(sizeof(char) * (len + 1));
+	if (!str)
+		return (NULL);
+	str[len] = '\0';
+	while (len--)
+	{
+		str[len] = nb % 10 + '0';
+		nb /= 10;
+		if (nb == 0 && sign)
+			break ;
+	}
+	if (sign)
+		str[0] = '-';
+	return (str);
 }
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kkaman <kkaman@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/23 14:48:10 by kkaman            #+#    #+#             */
+/*   Updated: 2025/10/23 14:49:23 by kkaman           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 char	*ft_strtrim(char const *s1, char const *set)
@@ -13,7 +25,7 @@ char	*ft_strtrim(char const *s1, char const *set)
 	while (s1[start] && ft_strchr(set, s1[start]))
 		start++;
 	end = ft_strlen(s1);
-	while (end > start && ft_strchr(set, s1[end-1]))
+	while (end > start && ft_strchr(set, s1[end - 1]))
 		end--;
 	return (ft_substr(s1, start, end - start));
 }
